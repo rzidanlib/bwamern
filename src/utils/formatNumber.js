81c57a5c3481970c1b401ca134eefae5bc1ed550
Zidan <library.devz@gmail.com>
2023-08-09 10:08:18 +0700

    Create Hero component, editing font style, and styling hero component

---
+++ src/utils/formatNumber.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default (number) => {
+  const formatNumbering = new Intl.NumberFormat('id-ID');
+  return formatNumbering.format(number);
+};
